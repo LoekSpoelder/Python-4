b6d32a6a11a2e3fa7112ad9a8dee4378e938030c
Loek Spoelder <loek.spoelder@coderclass.nl>
2023-01-30 15:45:37 +0100

    Had to restart- using chat GPT is too easy.

--- program.py
+++ program.py
@@ -1,55 +1,10 @@
-# Create a dictionary to store the word pairs
-words = {}
-
-# Function to add new words to the dictionary
-def add_word(native, foreign):
-    words[native] = foreign
-
-# Function to translate a native word to foreign
-def translate(native):
-    if native in words:
-        return words[native]
-    else:
-        return "Word not found."
-
-# Function to test user's knowledge of the words
-def test():
-    score = 0
-    for native, foreign in words.items():
-        answer = input(f"Translate {native}: ")
-        if answer == foreign:
-            print("Correct!")
-            score += 1
-        else:
-            print(f"Incorrect. The correct answer is {foreign}.")
-    print(f"You scored {score} out of {len(words)}.")
-
-# Function to revise words
-def revise():
-    for native, foreign in words.items():
-        print(f"{native} : {foreign}")
-
-# Add some words to the dictionary
-add_word("book", "livre")
-add_word("table", "table")
-add_word("car", "voiture")
-
-while True:
-    print("Welcome to the Word Reviewer.")
-    print("1. Test your knowledge")
-    print("2. Revise words")
-    print("3. Add new word")
-    print("4. Exit")
-    choice = input("Enter your choice: ")
-    if choice == "1":
-        test()
-    elif choice == "2":
-        revise()
-    elif choice == "3":
-        native = input("Enter native word: ")
-        foreign = input("Enter foreign word: ")
-        add_word(native, foreign)
-    elif choice == "4":
-        exit()
-    else:
-        print("Invalid choice. Please enter a number between 1 and 4.")
+import time
+print("-"*40)
+print(f"|{'Menu':^38}|")
+print("|" + " "*38 + "|")
+print(f"| {'1. Test your knowledge':<36} |")
+print(f"| {'2. Revise for knowledge':<36} |")
+print(f"| {'3. Give me your knowledge':<36} |")
+print(f"| {'4. End our knowledge':<36} |")
+print("-"*40)
+choice = input("Uw keuze: ")
