378905958e427aa69b612141b156a870b94f827c
Loek Spoelder <loek.spoelder@coderclass.nl>
2023-02-09 15:37:33 +0100

    test

--- test.py
+++ test.py
@@ -1,8 +1,14 @@
-with open("stowage.txt") as f:
-  bestandsdata = f.read().split("\n")
+#with open("stowage.txt") as f:
+#  bestandsdata = f.read().split("\n")
+#for item in bestandsdata:
+#  woord1, woord2 = item.split("=")
+#print(woord1)
+#print(woord2)
 
-for item in bestandsdata:
-    woord1, woord2 = item.split("=")
-
-print(woord1)
-print(woord2)
+#import time
+#f = open('stowage.txt')
+#for line in f:
+#  woord1, woord2 = line.strip('\n').split('=')
+#  print(woord1 + " en " + woord2)
+#  time.sleep(0.5)
+#f.close('stowage.txt')
